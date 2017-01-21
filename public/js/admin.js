bdcfd3061630782425f71bcc5c7805f7892efffd
yourlei <yourlin127@gmail.com>
2017-01-21 23:14:16 +0800

    aside nav animate

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,5 +1,35 @@
 $(document).ready(function() {
-	$('#avatar').on('mouseover', function (e) {
-		$(this).velocity('callout.shake', {duration: 1000});
+	// user avatar animate when mouseover
+	$("#avatar").on("mouseover", function (e) {
+		$(this).velocity("callout.shake", {duration: 1000});
 	});
+	// toggle seconde items when click first-item
+	$(".first-item").click(function (event) {
+		event.preventDefault();
+		var _this = $(this),
+				index = $(this).parent().index(),
+				secondItems = $(".second-items");
+
+		if (_this.is('.actived-item')) 
+		{
+			_this.siblings(".second-items").toggle('slow');
+			return;
+		}
+
+		$('.first-item').removeClass('actived-item');
+		_this.addClass('actived-item');
+
+		if(secondItems.attr("display") != "none")
+		{
+			secondItems.slideUp();
+		}
+		if (_this.siblings())
+		{
+			_this.siblings(".second-items").slideDown();
+		}
+	});
+	/*$('.actived-item').click(function (event) {
+		event.preventDefault();
+		$(this).toggle();
+	});*/
 });
